1639889ca5b212c476b62937efad197c0b5edfa5
Herve Pages <hpages@fhcrc.org>
2011-10-26 23:27:02 +0000

    Address 'R CMD check' warning and also one of the two notes.
    git-svn-id: file:///home/git/bioc-data.hedgehog.fhcrc.org/trunk/experiment/pkgs/ccTutorial@1807 db2202da-8704-0410-b924-b125b3cd84ef

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 
 .onLoad <- function(libname, pkgname) {
     
-  if(.Platform$OS.type == "windows" && require(Biobase) && interactive()
+  if(.Platform$OS.type == "windows" && interactive()
      && .Platform$GUI ==  "Rgui"){
     addVigs2WinMenu("ccTutorial")
     addVigs2WinMenu("ccTutorialSupplement")
